4af57c40c21d5292ff834e3fd6db93cde8752b9a
Mmabatho Manogo <jennifermabatho7@gmail.com>
2023-05-28 19:26:34 +0200

    First Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtHoursWorked = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.txtRate = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtHoursWorked
+        '
+        Me.txtHoursWorked.Location = New System.Drawing.Point(106, 39)
+        Me.txtHoursWorked.Name = "txtHoursWorked"
+        Me.txtHoursWorked.Size = New System.Drawing.Size(100, 20)
+        Me.txtHoursWorked.TabIndex = 0
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(106, 13)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(100, 20)
+        Me.txtSurname.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Surname :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Hours Worked :"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(8, 241)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(8, 270)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(8, 299)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 6
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(12, 109)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(194, 95)
+        Me.lstOutput.TabIndex = 7
+        '
+        'txtRate
+        '
+        Me.txtRate.Location = New System.Drawing.Point(106, 65)
+        Me.txtRate.Name = "txtRate"
+        Me.txtRate.Size = New System.Drawing.Size(100, 20)
+        Me.txtRate.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Rate :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 334)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtRate)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSurname)
+        Me.Controls.Add(Me.txtHoursWorked)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtHoursWorked As System.Windows.Forms.TextBox
+    Friend WithEvents txtSurname As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents txtRate As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,117 @@
+﻿
+Option Explicit On
+Option Strict On
+
+
+
+
+Public Class Form1
+
+    Private Const NormalHours As Double = 40
+
+    Private Function CalcOvertimeHours(ByVal Hours As Double) As Double
+
+        'Declare Variables
+
+        Dim Overtime As Double
+
+        'Process 
+
+        If Hours > NormalHours Then
+
+            Overtime = Hours - NormalHours
+
+        Else
+
+            Overtime = 0
+
+        End If
+
+        Return Overtime
+
+
+    End Function
+
+
+    Private Function CalcPay(ByVal Hours As Double, ByVal OverTimeHours As Double, ByVal Rate As Decimal) As Decimal
+
+        'Declare Variables
+
+        Dim Pay As Decimal
+
+        'Process 
+
+        If Hours <= NormalHours Then
+
+            Pay = CDec(Hours * Rate)
+
+        Else
+
+            Pay = CDec((NormalHours * Rate) + (OverTimeHours * Rate * 2))
+
+
+        End If
+
+        Return Pay
+
+    End Function
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        'Declare Variables
+
+        Dim Surname As String
+        Dim Hours As Double
+        Dim OverTimeHours As Double
+        Dim Rate As Decimal
+        Dim Pay As Decimal
+
+        'Get and Convert INPUT 
+
+        Surname = txtSurname.Text
+        Hours = CDbl(txtHoursWorked.Text)
+        Rate = CDec(txtRate.Text)
+
+        'Calculate The Overtime Hours (Fuction Call)
+
+        OverTimeHours = CalcOvertimeHours(Hours)
+
+        'Calculate The Pay (Function Call)
+
+        Pay = CalcPay(Hours, OverTimeHours, Rate)
+
+        'Display Output
+
+        lstOutput.Items.Clear() 'Clear any previous content
+
+        lstOutput.Items.Add("********Employee*********")
+        lstOutput.Items.Add("Surname :" & Surname)
+        lstOutput.Items.Add("Overtime Hours :" & OverTimeHours.ToString("N1"))
+        lstOutput.Items.Add("Pay :" & Pay.ToString("C2"))
+        lstOutput.Items.Add("*************************")
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'Clear Variables
+
+        txtHoursWorked.Clear()
+        txtRate.Clear()
+        txtSurname.Clear()
+        lstOutput.Items.Clear()
+
+        txtSurname.Focus()
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+
+        'Close Program 
+
+        Me.Close()
+
+
+    End Sub
+End Class
